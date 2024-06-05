56401aca3c457e157cbc66038e7aa024249a4702
DoIkk <151039498+DoIkk@users.noreply.github.com>
2024-06-05 16:49:31 +0900

    Update face.py
    bounding box 캡쳐추가

--- doik/face.py
+++ doik/face.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 from ultralytics import YOLO
 import cv2
+import os
 
 # YOLO 모델 로드
 model = YOLO("C:/Users/doik0/Desktop/yolo/runs/detect/train3/weights/best.pt")
@@ -16,7 +17,12 @@ classes_info = {
 }
 
 # 신뢰도 임계값
-confidence_threshold = 0.75
+confidence_threshold = 0.9
+
+detection_counter = {0: 0, 1: 0, 2: 0, 3: 0}
+
+capture_path = "C:/Users/doik0/Desktop/yolo/captures" # 이미지 저장 경로
+os.makedirs(capture_path, exist_ok=True) 
 
 def use_result(results, frame):
     if results and results[0]:
@@ -32,16 +38,36 @@ def use_result(results, frame):
             else:
                 detected_classes.add(cls)
                 class_info = classes_info.get(cls, {"name": "Unknown", "color": (0, 255, 0)})  # Default color green for unknown classes
-            
+            detection_counter[cls] += 1
+
             (x, y, x2, y2) = bbox
             print(f"bounding box ({x}, {y}, {x2}, {y2}) has class {cls}, which is {class_info['name']}, confidence score: {score:.2f}")
             color = class_info["color"]
             cv2.rectangle(frame, (x, y), (x2, y2), color, 2)
-            cv2.putText(frame, class_info["name"], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
-        
+            
+            # 이름과 신뢰도 점수를 함께 표시
+            label = f"{class_info['name']}: {score:.2f}"
+            cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
+
         # 탐지된 고유한 클래스 수 출력
         print(f"Number of detected classes: {len(detected_classes)}")
+        
+        # 특정 횟수에 도달했을 때 이미지 저장
+        for cls, count in detection_counter.items():
+            if count == 100: 
+                # 바운딩 박스를 기준으로 이미지를 잘라내기
+                for cls, bbox in zip(classes, bboxes):
+                    if cls in detected_classes:
+                        (x, y, x2, y2) = bbox
+                        cropped_image = frame[y:y2, x:x2]
+                        cropped_filename = f"cropped_class_{cls}_capture.jpg" # 이미지 파일 이름
+                        cropped_filepath = os.path.join(capture_path, cropped_filename)
+                        cv2.imwrite(cropped_filepath, cropped_image)
+                        print(f"Captured and saved cropped image for class {cls} at {cropped_filepath}")
 
+                # 카운터 초기화
+                detection_counter[cls] = 0
+        
         scale_percent = 60  # 원래 크기의 백분율
         width = int(frame.shape[1] * scale_percent / 100)
         height = int(frame.shape[0] * scale_percent / 100)
