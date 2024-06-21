de6540d10195f530340a0f8350b2febdcb655eea
Kim, Isu <49092508+isu-kim@users.noreply.github.com>
2024-06-22 03:56:45 +0900

    Add sleep and yawn
    - feature: add sleep and yawn detection
    - code from Kim Minjung
    
    Co-Authored-By: MINJUNG KIM <95292485+eggplantgf@users.noreply.github.com>

--- engagement-recogntion-minjung-and-shokhrukh/Drowsiness_Detection.py
+++ engagement-recogntion-minjung-and-shokhrukh/Drowsiness_Detection.py
@@ -1,8 +1,31 @@
+"""
+ Code by Kim Minjung and Shokhrukh Talatov
+"""
+
 from scipy.spatial import distance
+from flask import Flask, request, jsonify
 from imutils import face_utils
 import imutils
+import base64
 import dlib
+import cmake
+import numpy as np
 import cv2
+
+
+app = Flask(__name__)
+
+thresh = 0.25  # 눈을 감은 것을 판단하는 threshold 값
+yawn_thresh = 20  # 하품을 판단하는 임계값
+detect = dlib.get_frontal_face_detector()  # 얼굴 검출기 생성
+predict = dlib.shape_predictor("./models/shape_predictor_68_face_landmarks.dat")
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+flag = 0  # 눈을 감은 프레임 수 초기화
+
+detector = cv2.CascadeClassifier("./yawn_models/yawn_plus.xml")
+predictor = dlib.shape_predictor("./yawn_models/shape_predictor_face_landmarks.dat")
+
 
 def eye_aspect_ratio(eye):  # 눈의 특징적 비율을 계산하는 함수
     A = distance.euclidean(eye[1], eye[5])
@@ -11,18 +34,44 @@ def eye_aspect_ratio(eye):  # 눈의 특징적 비율을 계산하는 함수
     ear = (A + B) / (2.0 * C)
     return ear
 
-thresh = 0.25  # 눈을 감은 것을 판단하는 threshold 값
-frame_check = 20  # 눈을 감은 것을 판단하는 프레임 수 설정
-detect = dlib.get_frontal_face_detector()  # 얼굴 검출기 생성
-predict = dlib.shape_predictor("C:\yolov8\sleepy\models\shape_predictor_68_face_landmarks.dat")
-(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
-(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
-cap = cv2.VideoCapture(0)  # 비디오 캡처 객체 생성
-flag = 0  # 눈을 감은 프레임 수 초기화
 
-while True:
-    ret, frame = cap.read()  # 비디오 프레임 읽기
-    frame = imutils.resize(frame, width=450)  # 프레임 크기 조정
+def lip_distance(shape):  # 하품을 감지하는 함수
+    top_lip = shape[50:53]
+    top_lip = np.concatenate((top_lip, shape[61:64]))
+    low_lip = shape[56:59]
+    low_lip = np.concatenate((low_lip, shape[65:68]))
+    top_mean = np.mean(top_lip, axis=0)
+    low_mean = np.mean(low_lip, axis=0)
+    distance = abs(top_mean[1] - low_mean[1])
+    return distance
+
+
+def base64_to_image(base64_string):
+    decoded_data = base64.b64decode(base64_string)
+    np_data = np.frombuffer(decoded_data, np.uint8)
+    image = cv2.imdecode(np_data, cv2.IMREAD_COLOR)
+
+    return image
+
+
+@app.route('/engagement', methods=['POST'])
+def process_pixels():
+    request_data = request.json
+
+    pixels_b64 = request_data.get('pixels_b_64', '')
+    frame = base64_to_image(pixels_b64)
+
+    remote_ip = request.remote_addr
+    print(f"Received pixels from {remote_ip}")
+
+    detect_sleepiness(frame)
+    detect_yawn(frame)
+
+    return jsonify({'message': 'Received and processed pixels successfully'})
+
+
+def detect_sleepiness(frame):
+    frame = imutils.resize(frame, width=450)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # 그레이스케일 이미지로 변환
     subjects = detect(gray, 0)  # 얼굴 검출
 
@@ -41,22 +90,38 @@ while True:
         cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
         cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
 
-        if ear < thresh:  # 눈을 감았을 때
-            flag += 1  # 프레임 수 증가
-            print(flag)  # 프레임 수 출력
-            if flag >= frame_check:  # 설정한 프레임 수 이상 눈을 감았을 때
-                cv2.putText(frame, "****************SLEEPY!****************", (10, 30), 
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-                cv2.putText(frame, "****************SLEEPY!****************", (10, 325),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-                #print("Drowsy")
-        else:
-            flag = 0  # 눈을 뜨면 프레임 수 초기화
+        if ear < thresh:  # 눈을 감은 경우
+            print("sleeping")
+        else:  # 눈을 뜬 경우
+            print("focusing")
 
-    cv2.imshow("Frame", frame)  # 프레임 화면에 출력
-    key = cv2.waitKey(1) & 0xFF  # 키 입력 대기
-    if key == ord("q"):  # 'q' 키를 누르면 종료
-        break
 
-cv2.destroyAllWindows()  # 모든 창 닫기
-cap.release()  # 비디오 캡처 해제
+def detect_yawn(frame):
+    frame = imutils.resize(frame, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # 그레이스케일 이미지로 변환
+    subjects = detect(gray, 0)  # 얼굴 검출
+
+    for subject in subjects:  # 검출된 얼굴마다 반복
+        shape = predict(gray, subject)
+        shape = face_utils.shape_to_np(shape)
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+        ear = (leftEAR + rightEAR) / 2.0
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+        # 하품 감지
+        distance = lip_distance(shape)
+        lip = shape[48:60]
+        cv2.drawContours(frame, [lip], -1, (0, 255, 0), 1)
+
+        if distance > yawn_thresh:  # 하품을 한 경우
+            print("yawned!")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)  # run api
---
+++ engagement-recogntion-minjung-and-shokhrukh/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 Drowsiness_Detection.py
